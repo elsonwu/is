5b3dab5e998fc47ff4333a31773521dcaa16f0a5
elsonwu <elsonwu@126.com>
2014-06-20 11:35:47 +0800

    add email/url validate function

---
+++ is.go
@@ -0,0 +1,18 @@
+package is
+
+import "regexp"
+
+const (
+	EmailPattern = `^[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$`
+	UrlPattern   = `((http|https|ftp){1}\:\/\/)?([a-zA-Z0-9\-\.]+[a-zA-Z0-9]+\.(net|cn|co|hk|tw|com|edu|gov|us|int|mil|org|int|mil|vg|uk|idv|tk|se|nz|nu|nl|ms|jp|jobs|it|ind|gen|firm|in|gs|fr|fm|eu|es|de|bz|be|at|am|ag|mx|asia|ws|xxx|tv|cc|ca|mobi|me|biz|arpa|info|name|pro|aero|coop|museum|ly|eg|mk)(:[a-zA-Z0-9]*)?\/?([a-zA-Z0-9\-\._\?\'\/\\\+&amp;%\$#\=~])*)+`
+)
+
+func Email(v string) bool {
+	ok, err := regexp.MatchString(EmailPattern, v)
+	return ok && err == nil
+}
+
+func Url(v string) bool {
+	ok, err := regexp.MatchString(UrlPattern, v)
+	return ok && err == nil
+}
