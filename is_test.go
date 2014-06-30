c9df621ec2bbcadc7d9aaa401036e20b5e19980c
elsonwu <elsonwu@126.com>
2014-06-30 15:34:20 +0800

    fix a url pattern issue

--- is_test.go
+++ is_test.go
@@ -19,6 +19,30 @@ func TestIsUrl(t *testing.T) {
 		t.Error("google.com - it should be a url")
 	}
 
+	if !Url("www.google.com/test/test.jpg") {
+		t.Error("google.com - it should be a url")
+	}
+
+	if !Url("www.google.com/test/test.jpg?v=123&v2=321") {
+		t.Error("google.com - it should be a url")
+	}
+
+	if !Url("http://www.google.com/test/test.jpg?v=123&v2=321") {
+		t.Error("google.com - it should be a url")
+	}
+
+	if !Url("http://www.google.com:8080/test/test.jpg?v=123&v2=321") {
+		t.Error("google.com - it should be a url")
+	}
+
+	if !Url("http://www.google.com:8080") {
+		t.Error("google.com - it should be a url")
+	}
+
+	if Url("2014/newimages/icon_15.jpg") {
+		t.Error("2014/newimages/icon_15.jpg - it should not be a url")
+	}
+
 	if Url("google_com") {
 		t.Error("google_com - it should not be a url")
 	}
