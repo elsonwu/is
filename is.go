194120925d059375a4249db3d42698b529231eed
elsonwu <elsonwu@126.com>
2014-07-10 10:28:41 +0800

    add Zero function

--- is.go
+++ is.go
@@ -1,6 +1,12 @@
 package is
 
-import "regexp"
+import (
+	"reflect"
+	"regexp"
+	"time"
+)
+
+var typeTime = reflect.TypeOf(time.Time{})
 
 const (
 	EmailPattern = `^[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+(?:\.[a-zA-Z0-9!#$%&\'*+\\/=?^_{|}~-]+)*@(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9-]*[a-zA-Z0-9])?$`
@@ -15,4 +21,37 @@ func Email(v string) bool {
 func Url(v string) bool {
 	ok, err := regexp.MatchString(UrlPattern, v)
 	return ok && err == nil
+}
+
+// borrow from "labix.org/v2/mgo/bson"
+func Zero(v reflect.Value) bool {
+	switch v.Kind() {
+	case reflect.String:
+		return len(v.String()) == 0
+	case reflect.Ptr, reflect.Interface:
+		return v.IsNil()
+	case reflect.Slice:
+		return v.Len() == 0
+	case reflect.Map:
+		return v.Len() == 0
+	case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+		return v.Int() == 0
+	case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+		return v.Uint() == 0
+	case reflect.Float32, reflect.Float64:
+		return v.Float() == 0
+	case reflect.Bool:
+		return !v.Bool()
+	case reflect.Struct:
+		if v.Type() == typeTime {
+			return v.Interface().(time.Time).IsZero()
+		}
+		for i := v.NumField() - 1; i >= 0; i-- {
+			if !Zero(v.Field(i)) {
+				return false
+			}
+		}
+		return true
+	}
+	return false
 }
